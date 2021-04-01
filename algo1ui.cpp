b1097cfd6cf143b119d327c36e575b0b3ff672f4
jsalig00 <36676028+Jsalig00@users.noreply.github.com>
2021-04-01 18:34:17 +0200

    Add files via upload

---
+++ algo1ui.cpp
@@ -0,0 +1,14 @@
+#include "algo1ui.h"
+#include "ui_algo1ui.h"
+
+algo1ui::algo1ui(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::algo1ui)
+{
+    ui->setupUi(this);
+}
+
+algo1ui::~algo1ui()
+{
+    delete ui;
+}
