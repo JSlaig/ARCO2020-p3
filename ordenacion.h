347fec2f896ab01517463a0cfe64cee4fe753f3d
infjmv00 <55315862+infjmv00@users.noreply.github.com>
2021-04-04 16:55:42 +0200

    subido nuevo archivo de ordenacion.h

--- ordenacion.h
+++ ordenacion.h
@@ -10,9 +10,10 @@ private:
     std::vector<int>listaNumeros;
 public:
     Ordenacion();
-    void rellenarLista();
+    void rellenarLista(std::string ruta);
     void ordenarLista();
     void imprimirLista();
+    void escribirFichero(std::string ruta);
 };
 
 #endif // ORDENACION_H
